1e43a30c6e43d2ffc15fcdf09a279e33b5d68463
Orka Arnest CRUZE <orka.cruze@ign.fr>
2021-01-11 15:36:03 +0100

    env config for travis

--- create-env-test.sh
+++ create-env-test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+# only for travis ci
 set -e
 
 touch .env.test
 
 echo "APP_ENV=test" >.env.test
-echo "DATABASE_URL=${DATABASE_URL}" >>.env.test
+echo "DATABASE_URL=postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?serverVersion=10&charset=utf8" >>.env.test
 echo "POSTGRES_USER=${POSTGRES_USER}" >>.env.test
 echo "POSTGRES_PASSWORD=${POSTGRES_PASSWORD}" >>.env.test
 echo "POSTGRES_DB=${POSTGRES_DB}" >>.env.test
