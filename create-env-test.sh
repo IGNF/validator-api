f6825a2c3329f47283c3a67fb2a9e06fa0d2757e
Orka Arnest CRUZE <orka.cruze@ign.fr>
2021-01-11 14:49:49 +0100

    travis config, trying again

--- create-env-test.sh
+++ create-env-test.sh
@@ -4,7 +4,10 @@ set -e
 
 touch .env.test
 
-echo "DATABASE_URL=${DATABASE_URL}" > .env.test
-echo "POSTGRES_USER=${POSTGRES_USER}" >> .env.test
-echo "POSTGRES_PASSWORD=${POSTGRES_PASSWORD}" >> .env.test
-echo "POSTGRES_DB=${POSTGRES_DB}" >> .env.test
+echo "APP_ENV=test" >.env.test
+echo "DATABASE_URL=${DATABASE_URL}" >>.env.test
+echo "POSTGRES_USER=${POSTGRES_USER}" >>.env.test
+echo "POSTGRES_PASSWORD=${POSTGRES_PASSWORD}" >>.env.test
+echo "POSTGRES_DB=${POSTGRES_DB}" >>.env.test
+
+cat .env.test
