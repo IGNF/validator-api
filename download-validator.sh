aa34abd806f98b4d60ecdd10e672920c6a12ff95
MBorne <mickael.borne@ign.fr>
2021-08-02 18:13:06 +0200

    upgrade validator cli to 4.2.0

--- download-validator.sh
+++ download-validator.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-VALIDATOR_VERSION=${VALIDATOR_VERSION:-4.1.0}
+VALIDATOR_VERSION=${VALIDATOR_VERSION:-4.2.0}
 URL="https://github.com/IGNF/validator/releases/download/v$VALIDATOR_VERSION/validator-cli.jar"
 
 echo "Downloading validator v${VALIDATOR_VERSION} from ${URL}..."
