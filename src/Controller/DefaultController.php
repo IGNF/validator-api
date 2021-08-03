42c195ee358b22de3875a24a89275ab2e3a88f9d
MBorne <mickael.borne@ign.fr>
2021-08-03 17:42:29 +0200

    avoid copy from docs/specs to public/docs using a dedicated DocumentationController (refs #11)

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -17,14 +17,4 @@ class DefaultController extends AbstractController
         return $this->render('demo.html.twig');
     }
 
-    /**
-     * Root path for the API displaying swagger.
-     *
-     * @Route("/validator/", name="validator_api_root")
-     */
-    public function apidoc()
-    {
-        return $this->render('swagger.html.twig');
-    }
-
 }
