c252f5bfc0170289fbfba4d69d9f1b0f829ea6bb
Damien D'ARRAS <damien.d-arras@ign.fr>
2024-11-29 17:41:27 +0100

    test: add health checks

---
+++ src/Controller/Api/HealthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use League\Flysystem\FilesystemOperator;
+
+
+/**
+ * @Route("/health")
+ */
+class HealthController extends AbstractController
+{
+    /**
+     * Checks for Database connection
+     *
+     * @Route("/db", name="heatlh_db")
+     */
+    public function headthDB(EntityManager $entityManager)
+    {
+        try{
+            $entityManager->getConnection()->connect();
+            $check = $entityManager->getConnection()->isConnected();
+            $httpCode = $check ? Response::HTTP_OK : Response::HTTP_SERVICE_UNAVAILABLE;
+            return new JsonResponse($check, $httpCode);
+        } catch (Exception $e){
+            return new JsonResponse(False, Response::HTTP_NOT_FOUND);
+        }
+    }
+
+        /**
+     * Checks for Database connection
+     *
+     * @Route("/s3", name="heatlh_s3")
+     */
+    public function headthS3(FilesystemOperator $dataStorage)
+    {
+        try {
+            $dataStorage->listContents('/', False);
+            return new JsonResponse(True, Response::HTTP_OK);
+        } catch (Exception $e) {
+            return new JsonResponse(False, Response::HTTP_NOT_FOUND);
+        }
+    }
+
+}
