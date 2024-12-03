8c06e0d8441bee151f49c1196d7383a3e77fadb9
MBorne <mickael.borne@ign.fr>
2024-12-03 12:13:12 +0100

    chore(health): log exceptions

--- src/Controller/Api/HealthController.php
+++ src/Controller/Api/HealthController.php
@@ -10,13 +10,17 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use League\Flysystem\FilesystemOperator;
-
+use Psr\Log\LoggerInterface;
 
 /**
  * @Route("/health")
  */
 class HealthController extends AbstractController
 {
+    public function __construct(private LoggerInterface $logger){
+
+    }
+
     /**
      * Checks for Database connection
      *
@@ -25,12 +29,16 @@ class HealthController extends AbstractController
     public function healthDB(EntityManagerInterface $entityManager)
     {
         $sql = "SELECT postgis_version() as postgis_version";
+        $this->logger->info('get postgis version',[
+            'sql' => $sql
+        ]);
         try{
             $stmt = $entityManager->getConnection()->prepare($sql);
             $result = $stmt->executeQuery();
             return new JsonResponse($result->fetchOne(), Response::HTTP_OK);
         } catch (Exception $e){
-            return new JsonResponse($e->getMessage(), Response::HTTP_NOT_FOUND);
+            $this->logger->error((string) $e);
+            return new JsonResponse($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -41,12 +49,14 @@ class HealthController extends AbstractController
      */
     public function healthS3(FilesystemOperator $dataStorage)
     {
+        $this->logger->info('list files from S3 bucket...');
         try {
             $files = $dataStorage->listContents('.', false);
             $numFiles = count($files->toArray());
             return new JsonResponse('found '.$numFiles.' files', Response::HTTP_OK);
         } catch (Exception $e) {
-            return new JsonResponse(False, Response::HTTP_NOT_FOUND);
+            $this->logger->error((string) $e);
+            return new JsonResponse("fail to list files from S3 bucket", Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
