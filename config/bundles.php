d0d724fdafc91cc9329fd55834d4c6cbd8fcb670
Damien D'ARRAS <156814279+DD-Arras@users.noreply.github.com>
2026-04-15 10:53:58 +0200

    Rapport pdf (#86)
    * pdf generator
    
    * feat: pdf rapport
    
    * feat: client for pdf rapport
    
    * fix: service knpsnappy

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
+    Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
 ];
